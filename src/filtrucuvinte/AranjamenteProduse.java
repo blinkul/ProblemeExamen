f610b67740d33de065127b38e5e6d21abbd72c6f
ciprian.anghel <anghelciprian91@gmail.com>
2019-08-02 12:35:32 +0300

    AranjamenteProduse

--- src/filtrucuvinte/AranjamenteProduse.java
+++ src/filtrucuvinte/AranjamenteProduse.java
@@ -1,63 +1,81 @@
 package filtrucuvinte;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class AranjamenteProduse {
 
-
-    // TODO: 10 4294967295 will fail
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+    
+        //true = will use hardcoded input instead of console; false for console input
+        boolean debug = true;
+        
+        String input;
 
-        String input = scanner.nextLine();
+        if (debug) {
+//             input = "10 10000"; //0
+             input = "5 1000000"; //17
+//             input = "4 900000"; //32
+        } else {
+            input = scanner.nextLine();
+        }
+
         long k = Long.valueOf(input.split(" ")[0]);
-        long M = Long.parseUnsignedLong(input.split(" ")[1]);
+        long M = Long.valueOf(input.split(" ")[1]);
 
         System.out.println(calculate(k,M));
-
-
-//        System.out.println(Long.MAX_VALUE);
-//        int i = 1;
-//
-//        while (i < 40) {
-//            System.out.println("i = " + i + "; Factorial = " + factorial(i));
-//
-//            i++;
-//        }
-
     }
 
     private static long calculate(long k, long M) {
+
+        // 1. k <= n; k [1,12]; M <= 4_294_967_295
+        // 2. A(n,k) = n! / (n-k)!
+
+        if (k < 1 || M < 0 || M > 4_294_967_295L || k > 12) {
+            return 0L;
+        }
+
+        // k <= n
         long n = k;
-        long result = 0;
 
-        result = factorial(n) / factorial(n-k);
+        boolean hasElements = false;
 
-        while (M < 4294967295L && result < M) {
+            while ( arrangements(n, k) <= 4_294_967_295L
+                    && arrangements(n, k) <= M) {
 
-            System.out.println("DEBUG: k = " + k + "; n = " + n);
-            result = factorial(n) / factorial(n-k);
-
-            if (result > M) {
-                return n - 1;
+                hasElements = true;
+                n++;
             }
 
-            n++;
-        }
+            if (!hasElements) return 0;
 
-        return 0;
+            // return previous n
+            return --n;
     }
 
-    private static long factorial(long n) {
+    private static String factorial(long n) {
 
-        long fact = 1;
+        BigInteger fact = new BigInteger("1");
 
         for(long i = 1; i <= n; i++) {
-            fact *= i;
+
+            BigInteger iBig = new BigInteger(String.valueOf(i));
+            fact = fact.multiply(iBig);
+
         }
 
-        return fact;
+        return fact.toString();
+    }
+
+    private static long arrangements(long n, long k) {
+
+        BigInteger a = new BigInteger(factorial(n));
+        BigInteger b = new BigInteger(factorial(n-k));
+
+        return Long.valueOf(a.divide(b).toString());
+
     }
 
 }
